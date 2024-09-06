817f6c003ea60a773c0b85ed2ddfe9d3916ce7ad
cconnors8 <caleb.connors27@gmail.com>
2024-09-06 16:16:19 -0400

    trial 1

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,8 +4,8 @@ query_file="$1"
 subject_file="$2"
 output_file="$3"
 
-blastn -query "$query_file" -subject "$subject_file" -outfmt "6 sseqid pident length qlen" > temp.tmp
-awk '$3 == $4 && $2 == 100' temp.tmp > "$output_file"
+blastp -query "$query_file" -subject "$subject_file" -outfmt "6 sseqid pident qlen length" > temp.tmp
+awk '$3 > $4 * 0.9 && $2 > 30' temp.tmp > "$output_file"
 count=$(wc -l < "$output_file")
 echo "Perfect Match Count: $count"
 
