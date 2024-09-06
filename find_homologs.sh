1994682363c2c2cdc8de416b9ead948c0717dea3
cconnors8 <caleb.connors27@gmail.com>
2024-09-06 16:28:44 -0400

    I cant read

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,7 +4,7 @@ query_file="$1"
 subject_file="$2"
 output_file="$3"
 
-blastp -query "$query_file" -subject "$subject_file" -outfmt "6 sseqid pident qlen length" > temp.tmp
+tblastn -query "$query_file" -subject "$subject_file" -outfmt "6 sseqid pident qlen length" > temp.tmp
 awk '$4 > $3 * 0.9 && $2 > 30' temp.tmp > "$output_file"
 count=$(wc -l < "$output_file")
 echo "Match Count: $count"
