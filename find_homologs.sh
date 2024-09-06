770695fddf5654f07d14a308929002a72169c9d9
cconnors8 <caleb.connors27@gmail.com>
2024-09-06 16:22:25 -0400

    word edits

--- find_homologs.sh
+++ find_homologs.sh
@@ -7,6 +7,6 @@ output_file="$3"
 blastp -query "$query_file" -subject "$subject_file" -outfmt "6 sseqid pident qlen length" > temp.tmp
 awk '$3 > $4 * 0.9 && $2 > 30' temp.tmp > "$output_file"
 count=$(wc -l < "$output_file")
-echo "Perfect Match Count: $count"
+echo "Match Count: $count"
 
 
